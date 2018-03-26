242cfac940e37f7fefd6141d624d2b30729913b2
Lewis Fong <fongl@bu.edu>
2018-03-26 14:08:31 -0700

    sketch work

---
+++ batoship.js
@@ -0,0 +1,88 @@
+var inq = require("inquirer");
+
+var map = {
+  width: 5,
+  height: 5,
+  playerLocations: [null, null],
+  turns: 1,
+  powerUp: [null,null]
+};
+
+(function initializePositions() {
+  starting = [[0, 0], [0, 4], [4, 0], [4, 4]];
+  let rando = Math.floor(Math.random() * starting.length);
+  map.playerLocations[0] = starting[rando];
+  starting.splice(rando, 1);
+  map.playerLocations[1] =
+    starting[Math.floor(Math.random() * starting.length)];
+})();
+
+console.log(map);
+action();
+function action() {
+  move();
+  map.turns++;
+  function move() {
+    var x;
+    if (map.turns % 2 != 0) {
+      x = 0;
+    } else {
+      x = 1;
+    }
+    var possible = [
+      map.playerLocations[x],
+      [map.playerLocations[x][0], map.playerLocations[x][1] + 1],
+      [map.playerLocations[x][0], map.playerLocations[x][1] - 1],
+      [map.playerLocations[x][0] + 1, map.playerLocations[x][1]],
+      [map.playerLocations[x][0] - 1, map.playerLocations[x][1]]
+    ];
+    let remove = [];
+    for (var i = 0; i < 5; i++) {
+      if (
+        possible[i][0] < 0 ||
+        possible[i][0] > 4 ||
+        possible[i][1] < 0 ||
+        possible[i][1] > 4
+      ) {
+        remove.push(i);
+      }
+    }
+    for (var i = remove.length - 1; i > -1; i--) {
+      possible.splice(remove[i], 1);
+    }
+    console.log(possible);
+    inq.prompt([{
+        type:"list",
+        message:"Where you wanna go?",
+        name: "location",
+        choices:possible
+    }]).then(function(movement){
+        map.playerLocations[x]=movement.location;
+        console.log(map);
+        attack();
+    });
+  }
+
+  function attack() {
+    var target;
+    if (map.turns % 2 != 0) {
+      if (target === map.playerLocations[1]) {
+        gameover(1);
+      } else {
+      }
+    } else {
+      if (target === map.playerLocations[0]) {
+        gameover();
+      } else {
+      }
+    }
+    
+    function gameover(input) {
+      if (input == 1) {
+        console.log("PLAYER 1 WINS");
+      } else {
+        console.log("PLAYER 2 WINS");
+      }
+    }
+  }
+}
