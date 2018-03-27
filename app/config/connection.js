8a9deda453277a5c16f48c9fca5362f762db1c41
Justin <justinho97@gmail.com>
2018-03-27 13:04:24 -0700

    env setup + creating schema

--- app/config/connection.js
+++ app/config/connection.js
@@ -3,11 +3,12 @@
 // *********************************************************************************
 
 // Dependencies
+require("dotenv").config();
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("starwars", "root", "ChangeMe123", {
-  host: "localhost",
+var sequelize = new Sequelize("battleship_DB", process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   pool: {
     max: 5,
