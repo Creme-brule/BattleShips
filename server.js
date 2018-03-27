9b64d5779609a3185fb47668d58d4b5a89e21822
Justin <justinho97@gmail.com>
2018-03-27 12:56:32 -0700

    server setup/ listening

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser =  require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//require("./app/routes/**INSERTPATH**")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening at PORT: " + PORT);
+});
