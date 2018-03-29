8f3ec20e1acd192623c5f097e311c6e2e280db9c
Yonghee Lee <poetkuma@gmail.com>
2018-03-29 10:29:03 -0700

    still needs some revision

--- app/models/map.js
+++ app/models/map.js
@@ -1,13 +1,13 @@
-var Sequelize = require ("sequelize");
-
-var sequelize = require ("../config/connection.js");
-
-    var Maps = sequelize.define("maps", {
+module.exports = function(sequelize, DataTypes) {
+  var Map = sequelize.define("Map", {
       id: Sequelize.INTEGER,
       size: Sequelize.INTEGER,
       shrinkRate: Sequelize.INTEGER,
       powerUp: Sequelize.INTEGER,
       shape: Sequelize.CHAR
-    });
-  
-module.exports = Maps;
+  });
+
+  return Map;
+}
+
+
